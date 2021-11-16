ab02c102d6340799931a62d211186dce6e73575e
arnab_sheikh <arnab.sheikh007@gmail.com>
2021-11-16 21:47:04 +0600

    Admin Controll Added

--- CVStorage/Controllers/AdminController.cs
+++ CVStorage/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CVStorage.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,34 @@ using System.Threading.Tasks;
 
 namespace CVStorage.Controllers
 {
+    [Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly IPersonRepo _personRepo;
+
+        public AdminController(IPersonRepo personRepo)
         {
-            return View();
+            this._personRepo = personRepo;
+        }
+
+        public IActionResult Pending()
+        {
+            var model = _personRepo.GetAllPersons();
+            return View(model);
+        }
+
+        public IActionResult AcceptedUsers()
+        {
+            var model = _personRepo.GetAllPersons();
+            return View(model);
+        }
+
+        public IActionResult AcceptUser(string ID)
+        {
+            var model = _personRepo.GetPerson(ID);
+            model.IsAccepted = true;
+            var person = _personRepo.Update(model);
+            return RedirectToAction("Pending", "Admin", new { area = "" });
         }
     }
 }
